0f9a01287f14722bd01290fbb89f647b5a685743
Prasannakumar1908 <prasanna@prodifyllp.com>
2024-11-05 19:16:21 +0530

    "Added eureka service registry"

--- GatewayService/src/main/java/com/prodify/cqrs/gatewayservice/GatewayserviceApplication.java
+++ GatewayService/src/main/java/com/prodify/cqrs/gatewayservice/GatewayserviceApplication.java
@@ -2,9 +2,11 @@ package com.prodify.cqrs.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class GatewayserviceApplication {
+@EnableDiscoveryClient
+public class GatewayserviceApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayserviceApplication.class, args);
